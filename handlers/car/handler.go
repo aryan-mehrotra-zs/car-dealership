1b09a68fc33789a7dd34a893b212408dc48722e8
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-26 01:06:53 +0530

    refactor code

--- handlers/car/handler.go
+++ handlers/car/handler.go
@@ -23,67 +23,6 @@ func New(service services.Car) handler {
 	return handler{service: service}
 }
 
-func getCar(r *http.Request) (models.Car, error) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return models.Car{}, err
-	}
-
-	var car models.Car
-
-	err = json.Unmarshal(body, &car)
-	if err != nil {
-		return models.Car{}, err
-	}
-
-	return car, nil
-}
-
-func setStatusCode(w http.ResponseWriter, err error, method string, data interface{}) {
-	switch err.(type) {
-	case errors.EntityAlreadyExists:
-		w.WriteHeader(http.StatusOK)
-	case errors.MissingParam, errors.InvalidParam:
-		w.WriteHeader(http.StatusBadRequest)
-	case nil:
-		writeSuccessResponse(method, w, data)
-	default:
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-}
-
-func writeSuccessResponse(method string, w http.ResponseWriter, data interface{}) {
-	switch method {
-	case http.MethodPost:
-		writeResponseBody(w, http.StatusCreated, data)
-	case http.MethodGet:
-		writeResponseBody(w, http.StatusOK, data)
-	case http.MethodPut:
-		writeResponseBody(w, http.StatusOK, data)
-	case http.MethodDelete:
-		writeResponseBody(w, http.StatusNoContent, data)
-	}
-}
-
-func writeResponseBody(w http.ResponseWriter, statusCode int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	resp, err := json.Marshal(data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
-	}
-
-	w.WriteHeader(statusCode)
-
-	_, err = w.Write(resp)
-	if err != nil {
-		log.Println("error in writing response")
-
-		return
-	}
-}
-
 func (h handler) Create(w http.ResponseWriter, r *http.Request) {
 	var car models.Car
 
@@ -94,19 +33,10 @@ func (h handler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	car.ID = uuid.New()
+
 	car, err = h.service.Create(car)
 	setStatusCode(w, err, r.Method, car)
-}
-
-func getID(r *http.Request) uuid.UUID {
-	param := mux.Vars(r)
-	idParam := param["id"]
-
-	// How to handle panics
-	// MustParse is like Parse but panics if the string cannot be parsed. It simplifies safe initialization of global variables holding compiled UUIDs.
-	id := uuid.MustParse(idParam)
-
-	return id
 }
 
 func (h handler) GetAll(w http.ResponseWriter, r *http.Request) {
@@ -172,4 +102,76 @@ func (h handler) Delete(w http.ResponseWriter, r *http.Request) {
 
 	err := h.service.Delete(id)
 	setStatusCode(w, err, r.Method, nil)
+}
+
+func getCar(r *http.Request) (models.Car, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return models.Car{}, err
+	}
+
+	var car models.Car
+
+	err = json.Unmarshal(body, &car)
+	if err != nil {
+		return models.Car{}, err
+	}
+
+	return car, nil
+}
+
+func setStatusCode(w http.ResponseWriter, err error, method string, data interface{}) {
+	switch err.(type) {
+	case errors.EntityAlreadyExists:
+		w.WriteHeader(http.StatusOK)
+	case errors.MissingParam, errors.InvalidParam:
+		w.WriteHeader(http.StatusBadRequest)
+	case nil:
+		writeSuccessResponse(method, w, data)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func writeSuccessResponse(method string, w http.ResponseWriter, data interface{}) {
+	switch method {
+	case http.MethodPost:
+		writeResponseBody(w, http.StatusCreated, data)
+	case http.MethodGet:
+		writeResponseBody(w, http.StatusOK, data)
+	case http.MethodPut:
+		writeResponseBody(w, http.StatusOK, data)
+	case http.MethodDelete:
+		writeResponseBody(w, http.StatusNoContent, data)
+	}
+}
+
+func writeResponseBody(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	resp, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(statusCode)
+
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Println("error in writing response")
+
+		return
+	}
+}
+
+func getID(r *http.Request) uuid.UUID {
+	param := mux.Vars(r)
+	idParam := param["id"]
+
+	// How to handle panics
+	// MustParse is like Parse but panics if the string cannot be parsed. It simplifies safe initialization of global variables holding compiled UUIDs.
+	id := uuid.MustParse(idParam)
+
+	return id
 }
