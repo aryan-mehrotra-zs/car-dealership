df3de409dc34890354e5582e56a1861a64757a49
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-25 14:45:08 +0530

    modify engine struct

--- models/engine.go
+++ models/engine.go
@@ -3,7 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type Engine struct {
-	ID           uuid.UUID `json:"-id"`
+	ID           uuid.UUID `json:"-"`
 	Displacement int       `json:"displacement"`
 	NCylinder    int       `json:"NCylinder"`
 	Range        int       `json:"range"`
