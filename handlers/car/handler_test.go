10e2b4387a72eb1cf0b34c05eaede699cac525b9
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-25 16:15:31 +0530

    refactor read body

--- handlers/car/handler_test.go
+++ handlers/car/handler_test.go
@@ -15,18 +15,35 @@ import (
 	"github.com/amehrotra/car-dealership/filters"
 )
 
-func initializeTest(method string, body io.Reader, pathParams map[string]string) (handler, *http.Request, *httptest.ResponseRecorder) {
+func initializeTest(method string, body io.Reader, queryParams map[string]string) (handler, *http.Request, *httptest.ResponseRecorder) {
 	h := New(mockService{})
 
 	req := httptest.NewRequest(method, "http://car", body)
-	r := mux.SetURLVars(req, pathParams)
+	r := mux.SetURLVars(req, queryParams)
 	w := httptest.NewRecorder()
 
 	return h, r, w
 }
 
+// should we do error handling here?
+func readBody(w *httptest.ResponseRecorder, t *testing.T) ([]byte, *http.Response) {
+	resp := w.Result()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("cannot read response")
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		t.Errorf("Error in closing body")
+	}
+
+	return body, resp
+}
+
 func TestHandler_Create(t *testing.T) {
-	resp := []byte(`("ID":"123e4567-e89b-12d3-a456-426614174000","Model":"BMW","YearOfManufacture":2022,"Brand":"BMW",
+	res := []byte(`("ID":"123e4567-e89b-12d3-a456-426614174000","Model":"BMW","YearOfManufacture":2022,"Brand":"BMW",
 					"FuelType":"Petrol",{"Displacement":20,"NCylinder":2,"Range":0}}`)
 
 	cases := []struct {
@@ -35,14 +52,13 @@ func TestHandler_Create(t *testing.T) {
 		car        []byte
 		statusCode int
 	}{
-		{"created successfully", bytes.NewReader(resp), resp, http.StatusCreated},
-		{"entity already exists", bytes.NewReader(resp), resp, http.StatusOK},
+		{"created successfully", bytes.NewReader(res), res, http.StatusCreated},
+		{"entity already exists", bytes.NewReader(res), res, http.StatusOK},
 		{"unmarshal error", bytes.NewReader([]byte(`Invalid Body`)), []byte(""), http.StatusBadRequest},
-		{"database error", bytes.NewReader([]byte(`{"Model":"BMW","YearOfManufacture":2022,"Brand":"BMW","FuelType":"Petrol",{20,2,0}}`)), []byte(""), http.StatusInternalServerError},
 		{"missing parameter", bytes.NewReader([]byte(`{"Model":"BMW","YearOfManufacture":2022,"Brand":"BMW"}`)), []byte(""), http.StatusBadRequest},
 		{"invalid parameter", bytes.NewReader([]byte(`{"Model":BMW,"YearOfManufacture":2022,"Brand":"BMW","FuelType":"Petrol",{20,2,0}}`)), []byte(""), http.StatusBadRequest},
 		{"unable to read body", mockReader{}, []byte(""), http.StatusBadRequest},
-		{"database connectivity error", bytes.NewReader(resp), resp, http.StatusInternalServerError},
+		{"database connectivity error", bytes.NewReader(res), res, http.StatusInternalServerError},
 	}
 
 	for i, tc := range cases {
@@ -50,17 +66,7 @@ func TestHandler_Create(t *testing.T) {
 
 		h.Create(w, r)
 
-		resp := w.Result()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("cannot read response")
-		}
-
-		err = resp.Body.Close()
-		if err != nil {
-			t.Errorf("Error in closing body")
-		}
+		body, resp := readBody(w, t)
 
 		if resp.StatusCode != tc.statusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
@@ -107,17 +113,7 @@ func TestHandler_GetAll(t *testing.T) {
 
 		h.GetAll(w, r)
 
-		resp := w.Result()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("cannot read response")
-		}
-
-		err = resp.Body.Close()
-		if err != nil {
-			return
-		}
+		body, resp := readBody(w, t)
 
 		if tc.statusCode != resp.StatusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
@@ -130,7 +126,7 @@ func TestHandler_GetAll(t *testing.T) {
 }
 
 func TestHandler_GetByID(t *testing.T) {
-	resp := []byte(`{"ID":"123e4567-e89b-12d3-a456-426614174000","Model":"BMW","YearOfManufacture":2022,"Brand":"BMW",
+	res := []byte(`{"ID":"123e4567-e89b-12d3-a456-426614174000","Model":"BMW","YearOfManufacture":2022,"Brand":"BMW",
 					"FuelType":"Petrol"}`)
 
 	cases := []struct {
@@ -139,7 +135,7 @@ func TestHandler_GetByID(t *testing.T) {
 		resp       []byte
 		statusCode int
 	}{
-		{"request successful", uuid.UUID{}, resp, http.StatusOK},
+		{"request successful", uuid.UUID{}, res, http.StatusOK},
 		{"entity not found", uuid.UUID{}, []byte(""), http.StatusBadRequest},
 		{"invalid id", uuid.Nil, []byte(""), http.StatusBadRequest},
 		{"database connectivity error", uuid.UUID{}, []byte(""), http.StatusInternalServerError},
@@ -150,17 +146,7 @@ func TestHandler_GetByID(t *testing.T) {
 
 		h.GetByID(w, r)
 
-		resp := w.Result()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Errorf("cannot read response")
-		}
-
-		err = resp.Body.Close()
-		if err != nil {
-			return
-		}
+		body, resp := readBody(w, t)
 
 		if tc.statusCode != resp.StatusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
@@ -180,13 +166,14 @@ func TestHandler_Update(t *testing.T) {
 		desc       string
 		id         uuid.UUID
 		body       io.Reader
+		resp       []byte
 		statusCode int
 	}{
-		{"entity updated successfully", uuid.UUID{}, bytes.NewReader(fields), http.StatusOK},
-		{"entity does not exist", uuid.Nil, bytes.NewReader([]byte("")), http.StatusNotFound},
-		{"unable to read body", uuid.UUID{}, mockReader{}, http.StatusBadRequest},
-		{"unmarshal error", uuid.UUID{}, bytes.NewReader([]byte("invalid Body")), http.StatusBadRequest},
-		{"database connectivity error", uuid.UUID{}, bytes.NewReader([]byte("")), http.StatusInternalServerError},
+		{"entity updated successfully", uuid.UUID{}, bytes.NewReader(fields), fields, http.StatusOK},
+		{"entity does not exist", uuid.Nil, bytes.NewReader([]byte("")), []byte(""), http.StatusNotFound},
+		{"unable to read body", uuid.UUID{}, mockReader{}, []byte(""), http.StatusBadRequest},
+		{"unmarshal error", uuid.UUID{}, bytes.NewReader([]byte("invalid Body")), []byte(""), http.StatusBadRequest},
+		{"database connectivity error", uuid.UUID{}, bytes.NewReader([]byte("")), []byte(""), http.StatusInternalServerError},
 	}
 
 	for i, tc := range cases {
@@ -194,10 +181,14 @@ func TestHandler_Update(t *testing.T) {
 
 		h.Update(w, r)
 
-		resp := w.Result()
+		body, resp := readBody(w, t)
 
 		if tc.statusCode != resp.StatusCode {
 			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, resp.StatusCode, tc.statusCode)
+		}
+
+		if reflect.DeepEqual(body, tc.resp) {
+			t.Errorf("\n[TEST %d] Failed. Desc : %v\nGot %v\nExpected %v", i, tc.desc, string(body), string(tc.resp))
 		}
 	}
 }
