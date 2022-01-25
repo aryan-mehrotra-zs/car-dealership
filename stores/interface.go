415d01a7f859c8829bd7bf979fca955d4474339f
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-25 17:20:22 +0530

    modify variable name for GetAll

--- stores/interface.go
+++ stores/interface.go
@@ -3,12 +3,13 @@ package stores
 import (
 	"github.com/google/uuid"
 
+	"github.com/amehrotra/car-dealership/filters"
 	"github.com/amehrotra/car-dealership/models"
 )
 
 type Car interface {
 	Create(car models.Car) (uuid.UUID, error)
-	GetAll(brand string, engine bool) ([]models.Car, error)
+	GetAll(filter filters.Car) ([]models.Car, error)
 	GetByID(id uuid.UUID) (models.Car, error)
 	Update(car models.Car) error
 	Delete(id uuid.UUID) error
