f3afc721ad3ff424d635e749ec1a2a416399f3fc
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-26 00:15:28 +0530

    implement create method

--- handlers/car/handler.go
+++ handlers/car/handler.go
@@ -1,8 +1,13 @@
 package car
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
+	"github.com/amehrotra/car-dealership/errors"
+	"github.com/amehrotra/car-dealership/models"
 	"github.com/amehrotra/car-dealership/services"
 )
 
@@ -14,8 +19,79 @@ func New(service services.Car) handler {
 	return handler{service: service}
 }
 
-func (h handler) Create(w http.ResponseWriter, r *http.Request) {
+func getCar(r *http.Request) (models.Car, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return models.Car{}, err
+	}
 
+	var car models.Car
+
+	err = json.Unmarshal(body, &car)
+	if err != nil {
+		return models.Car{}, err
+	}
+
+	return car, nil
+}
+
+func setStatusCode(w http.ResponseWriter, err error, method string, data interface{}) {
+	switch err.(type) {
+	case errors.EntityAlreadyExists:
+		w.WriteHeader(http.StatusOK)
+	case errors.MissingParam, errors.InvalidParam:
+		w.WriteHeader(http.StatusBadRequest)
+	case nil:
+		writeSuccessResponse(method, w, data)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func writeSuccessResponse(method string, w http.ResponseWriter, data interface{}) {
+	switch method {
+	case http.MethodPost:
+		writeResponseBody(w, http.StatusCreated, data)
+	case http.MethodGet:
+		writeResponseBody(w, http.StatusOK, data)
+	case http.MethodPut:
+		writeResponseBody(w, http.StatusOK, data)
+	case http.MethodDelete:
+		writeResponseBody(w, http.StatusNoContent, data)
+	}
+}
+
+func writeResponseBody(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	resp, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(statusCode)
+
+	_, err = w.Write(resp)
+	if err != nil {
+		log.Println("error in writing response")
+
+		return
+	}
+}
+
+func (h handler) Create(w http.ResponseWriter, r *http.Request) {
+	var car models.Car
+
+	car, err := getCar(r)
+	if err != nil {
+		setStatusCode(w, err, r.Method, car)
+
+		return
+	}
+
+	car, err = h.service.Create(car)
+	setStatusCode(w, err, r.Method, car)
 }
 
 func (h handler) GetAll(w http.ResponseWriter, r *http.Request) {
