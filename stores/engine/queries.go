4459fbc9bce6ae2736200c6b964497996392845f
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-28 23:35:15 +0530

    modify update engine query

--- stores/engine/queries.go
+++ stores/engine/queries.go
@@ -3,6 +3,6 @@ package engine
 const (
 	insertEngine = "INSERT INTO engines (id,displacement,no_of_cylinder,`range`) VALUES (?,?,?,?)"
 	getEngine    = "SELECT * FROM engines WHERE id=?"
-	updateEngine = "UPDATE engines SET `displacement=?,no_of_cylinder=?,`range`=?` WHERE id=?"
+	updateEngine = "UPDATE engines SET displacement=?,no_of_cylinder=?,`range`=? WHERE id=?"
 	deleteEngine = "DELETE FROM engines WHERE id = ?;"
 )
