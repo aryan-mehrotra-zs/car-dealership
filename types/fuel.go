8a81a0fcc451b32b84c1b92068f87de98b2d2cc7
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-28 14:47:58 +0530

    implement value and scan method for custom type

--- types/fuel.go
+++ types/fuel.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"database/sql/driver"
+)
+
 type Fuel int
 
 const (
@@ -7,3 +11,16 @@ const (
 	Petrol
 	Electric
 )
+
+func (f Fuel) Value() (driver.Value, error) {
+	return int64(f), nil
+}
+
+func (f *Fuel) Scan(value interface{}) error {
+	switch value {
+	case "petrol":
+		*f = Fuel(1)
+		return nil
+	}
+	return nil
+}
