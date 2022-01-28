cbe4ec6a57eb7b24b995935ce7347a13e54e967c
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-01-28 23:30:47 +0530

    modify query to update car

--- stores/car/queries.go
+++ stores/car/queries.go
@@ -5,6 +5,6 @@ const (
 	getCars          = "SELECT * FROM cars;"
 	getCarsWithBrand = "SELECT * FROM cars WHERE brand=?;"
 	getCar           = "SELECT * FROM cars WHERE id = ?;"
-	updateCar        = "UPDATE cars SET `model=?,year_of_manufacture=?,brand=?,fuel_type=?,engine_id=?` WHERE id=?"
+	updateCar        = "UPDATE cars SET model=?,year_of_manufacture=?,brand=?,fuel_type=?,engine_id=? WHERE id=?"
 	deleteCar        = "DELETE FROM cars WHERE id=?;"
 )
