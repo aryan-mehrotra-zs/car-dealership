50ab921a21b20c514e028bd2f3a27fbaf4eeea63
aryan-mehrotra-zs <aryan.mehrotra@zopsmart.com>
2022-02-03 16:51:38 +0530

    implement custom marshler and unmarshler

--- types/fuel.go
+++ types/fuel.go
@@ -2,6 +2,10 @@ package types
 
 import (
 	"database/sql/driver"
+	"encoding/json"
+	"strings"
+
+	"github.com/amehrotra/car-dealership/errors"
 )
 
 type Fuel int
@@ -11,6 +15,42 @@ const (
 	Petrol
 	Electric
 )
+
+func (f Fuel) MarshalJSON() ([]byte, error) {
+	var s string
+	switch f {
+	case Petrol:
+		s = "petrol"
+	case Diesel:
+		s = "diesel"
+	case Electric:
+		s = "electric"
+	default:
+		return nil, errors.InvalidParam{Param: []string{"fuelType"}}
+	}
+
+	return json.Marshal(s)
+}
+
+func (f *Fuel) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	switch strings.ToLower(s) {
+	case "petrol":
+		*f = Petrol
+	case "diesel:":
+		*f = Diesel
+	case "electric":
+		*f = Electric
+	default:
+		return errors.InvalidParam{Param: []string{"fuelType"}}
+	}
+
+	return nil
+}
 
 func (f Fuel) Value() (driver.Value, error) {
 	return int64(f), nil
@@ -28,5 +68,6 @@ func (f *Fuel) Scan(value interface{}) error {
 		*f = Fuel(3)
 		return nil
 	}
+
 	return nil
 }
